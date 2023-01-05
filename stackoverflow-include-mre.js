2ebefae6fb586373b085abfad9caf1543eb22b8a
James Goodall <jfg@rincon.com>
2023-01-05 11:00:12 -0700

    add cursor positioning and keyboard event

--- stackoverflow-include-mre.js
+++ stackoverflow-include-mre.js
@@ -1,4 +1,9 @@
 javascript:(function() {
-    let id = location.pathname.split('/')[2];
-    document.activeElement.setRangeText("Please [edit](https://stackoverflow.com/posts/"+id+"/edit) your question to include a [minimum reproducible example](https://stackoverflow.com/help/minimal-reproducible-example) with test data demonstrating your problem.");
+    const txt = document.activeElement;
+    if (txt.nodeName === "TEXTAREA") {
+        const id = location.pathname.split('/')[2];
+        txt.value = "Please [edit](https://stackoverflow.com/posts/"+id+"/edit) your question to include a [minimum reproducible example](https://stackoverflow.com/help/minimal-reproducible-example) with test data demonstrating your problem.";
+        txt.setSelectionRange(txt.value.length, txt.value.length);
+        txt.dispatchEvent(new KeyboardEvent("keyup"));
+    }
 })();
