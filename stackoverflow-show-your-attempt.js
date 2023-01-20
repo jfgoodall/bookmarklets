3a22db748a2c016b861b63cd9be84f544d89ef57
James Goodall <jfg@rincon.com>
2023-01-20 13:45:45 -0700

    new bookmarklet

---
+++ stackoverflow-show-your-attempt.js
@@ -0,0 +1,9 @@
+javascript:(function() {
+    const txt = document.activeElement;
+    if (txt.nodeName === "TEXTAREA") {
+        const id = location.pathname.split('/')[2];
+        txt.value = "Can you please [edit](https://stackoverflow.com/posts/"+id+"/edit) your question to include what you have tried so far to solve the issue? See: [How to ask a good question](https://stackoverflow.com/help/how-to-ask).";
+        txt.setSelectionRange(txt.value.length, txt.value.length);
+        txt.dispatchEvent(new KeyboardEvent("keyup"));
+    }
+})();
